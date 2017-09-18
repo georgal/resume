de5f6eb74ea45c72e2988615e29401ded5134cf4
George <george.georgalidis@gmail.com>
2017-09-19 00:19:30 +0300

    Update contact.php

--- htdocs/contact.php
+++ htdocs/contact.php
@@ -1,12 +1,8 @@
 <?php
 /* e-mail recipient */
-$myemail  = "george.georgalidis@gmail.com";
-
-/* Check all form inputs using check_input function */
-$yourname = check_input($_POST['name']);
-$email    = check_input($_POST['mail']);
+$to  = "george.georgalidis@gmail.com";
 $subject  = 'Resume contact form';
-$textfield = check_input($_POST['message']);
+
 
 /* Let's prepare the message for the e-mail */
 $message = "Hello!
@@ -16,26 +12,9 @@ Your contact form has been submitted by:
 Name: $yourname
 E-mail: $email
 
-Message:
-$textfield
-
-End of message
-";
+Message='Hi there';
 
 /* Send the message using mail() function */
-mail($myemail, $subject, $message);
-
-/* Functions we used */
-function check_input($data, $problem='')
-{
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    if(($problem && strlen($data) == 0)!=1)
-    {
-        return $data;
-    }
-}
-
+mail($to, $subject, $message);
 
 ?>
