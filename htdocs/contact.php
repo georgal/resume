7a15db95191fec6a60cef8a02bdd0b9b589895a5
George <george.georgalidis@gmail.com>
2017-10-03 03:49:53 +0300

    Update contact.php

--- htdocs/contact.php
+++ htdocs/contact.php
@@ -1,58 +1,18 @@
 <?php 
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
 
-// define variables and set to empty values
-$name_error = $email_error= "";
-$name = $email = $message = $success = "";
+# Add Mailgun and Guzzle6 as a dependency (see Github README below for more info)
+php composer.phar require mailgun/mailgun-php php-http/guzzle6-adapter php-http/message
 
-//form is submitted with POST method
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $name_error = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-      $name_error = "Only letters and white space allowed"; 
-    }
-  }
+require 'vendor/autoload.php';
+use Mailgun\Mailgun;
+$mailgun = new Mailgun('key-274c0454bfb273b534863c15435be750', new \Http\Adapter\Guzzle6\Client());
 
-  if (empty($_POST["email"])) {
-    $email_error = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $email_error = "Invalid email format"; 
-    }
-  }
-
-  if (empty($_POST["message"])) {
-    $message = "";
-  } else {
-    $message = test_input($_POST["message"]);
-  }
-  
-  if ($name_error == '' and $email_error == ''){
-      $message_body = '';
-      unset($_POST['submit']);
-      foreach ($_POST as $key => $value){
-          $message_body .=  "$key: $value\n";
-      }
-      
-      $to = 'george.georgalidis@gmail.com';
-      $subject = 'Resume Form';
-      if (mail($to, $subject, $message)){
-          $success = "Message sent, thank you for contacting us!";
-          $name = $email = $message = '';
-      }
-  }
-  
-}
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
+# Make the call to the client.
+$result = $mgClient->sendMessage("sandbox8e91abbdc2b840a0aecbea08172c53e7.mailgun.org",
+  array('from'    =>  name,
+        'to'      => 'Mailgun Devs <devs@mailgun.net>',
+        'subject' => 'Resume Form',
+        'text'    => message));
 ?>
